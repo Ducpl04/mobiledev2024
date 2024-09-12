49f7c732799c2aa76e0083b6ab28ab05e6c01a61
Quy Duc <ducplq.22bi13096@usth.edu.vn>
2024-09-13 02:33:58 +0700

    prac7+prac8

--- USTH_weather/app/src/main/java/vn/usth/edu/weather/WeatherActivity.java
+++ USTH_weather/app/src/main/java/vn/usth/edu/weather/WeatherActivity.java
@@ -1,11 +1,22 @@
 package vn.usth.edu.weather;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.app.Fragment;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class WeatherActivity extends AppCompatActivity {
+    private ViewPager2 viewPager2;
+    private TabLayout tabLayout;
     public WeatherActivity() {
         super();
     }
@@ -21,9 +32,27 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i("onCreate", "onCreate State");
         setContentView(R.layout.activity_main);
         // Create a new Fragment to be placed in the activity
-        ForecastFragment firstFragment = new ForecastFragment();
-        // Add the fragment to the 'container' FrameLayout
-        getSupportFragmentManager().beginTransaction().add(R.id.main, firstFragment).commit();
+
+        viewPager2 = findViewById(R.id.viewPager);
+        HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(this);
+        viewPager2.setAdapter(adapter);
+        tabLayout = findViewById(R.id.tabLayout);
+        new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
+            switch (position){
+                case 0:
+                    tab.setText("Default");
+                    break;
+                case 1:
+                    tab.setText("Paris");
+                    break;
+                case 2:
+                    tab.setText("Hanoi");
+                    break;
+                case 3:
+                    tab.setText("Toulouse");
+                    break;
+            }
+        }).attach();
     }
 
     @Override
